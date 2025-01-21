7d218e07eaf65bf126fa30976723cce3f97be601
nick-vasiliev <nickthasim@gmail.com>
2025-01-21 22:17:56 +1100

    return json response

--- api/api.js
+++ api/api.js
@@ -4,14 +4,20 @@ var cors = require('cors')
 const app = express();
 const PORT = 9002;
 
+var question = {
+    id:1,
+    text:"What is a cat called?",
+    choices:["Cat","Dog","Steven","Schweppe"]
+}
+
 app.get('/answers', (req, res)=>{
     res.status(200);
-    res.send("welcome");
+    res.send({"alan":"answer"});
 });
 
 app.get('/question', cors(), (req, res)=>{
     res.status(200);
-    res.send("welcome");
+    res.send(question);
 });
 
 app.listen(PORT, (error) =>{
