7d87c0d582c0a5cf30289f28196ce3ca23561fa7
nick-vasiliev <nickthasim@gmail.com>
2025-02-01 00:51:26 +1100

    classes player, game

--- host/main.py
+++ host/main.py
@@ -1,14 +1,28 @@
 import asyncio
 from websockets.asyncio.server import serve
 
+class Player:
+    def __init__(self, name):
+        self.score = 0
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}: {self.score}"
+
+class Game:
+    def __init__(self):
+        self.in_progress = False
+        self.code = "NICK"
+        self.players = []
 
 async def handle_conn(websocket):
     async for message in websocket:
         # TODO: authentication - certificates? to authenticate different connections. For now, assume trust
         if message.startswith("SCREEN"):
-            print(f"SCREEN: {message}")
-        else:
-            print(f"PLAYER: {message}")
+            print(f"SCREEN: {message[6:]}")
+            return
+        # Player
+        print(message)
 
         await websocket.send(message)
 
