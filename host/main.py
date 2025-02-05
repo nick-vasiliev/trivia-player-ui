61231639a960175381401ea38d2c44884b5a860a
nick-vasiliev <nickthasim@gmail.com>
2025-02-05 21:32:25 +1100

    broadcast start

--- host/main.py
+++ host/main.py
@@ -1,12 +1,16 @@
 import asyncio
-from websockets.asyncio.server import serve
+from websockets.asyncio import server
 import json
 import game
 
-async def run() -> None: # TODO: This eventually has to be built concurrent for many simultaneous games.
+async def run(game_server) -> None: # TODO: This eventually has to be built concurrent for many simultaneous games, running_game would become a parameter.
     """Run Game loop."""
     while not running_game.in_progress:
         await asyncio.sleep(1) # await to allow other tasks to go
+    print("Starting")
+    await server.broadcast(game_server.connections, message='{"action":"game status","game_status":"Begin"}', raise_exceptions=True) # TODO: unset raise
+    # TODO: currently this sends many times
+    print("Done")
 
 async def handle_conn(websocket): # TODO: authentication - certificates? to authenticate different connections. For now, assume trust
     async for message in websocket: # websocket has an id
@@ -18,9 +22,9 @@ async def handle_conn(websocket): # TODO: authentication - certificates? to auth
         finally:
             await websocket.send(json.dumps(message_respond))
 async def main():
-    game_task = asyncio.create_task(run())
-    async with serve(handle_conn, "0.0.0.0", 8765) as server:
-        await server.serve_forever()
+    async with server.serve(handle_conn, "0.0.0.0", 8765) as game_server:
+        game_task = asyncio.create_task(run(game_server))
+        await game_server.serve_forever()
 
 if __name__ == "__main__":
     running_game = game.Game("NICK",123)
