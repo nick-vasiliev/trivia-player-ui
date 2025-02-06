d50397aaf44237ae47c4dcc5a4ea25752914e5ef
nick-vasiliev <nickthasim@gmail.com>
2025-02-06 23:21:24 +1100

    fixed loading many message

--- universal-remote/src/App.tsx
+++ universal-remote/src/App.tsx
@@ -3,25 +3,33 @@ import './App.css';
 import { CustomSender } from './components/CustomSender.tsx';
 import { DisplayMessages } from './components/DisplayMessages.tsx';
 
-function App() {
-  const [ws_responses, set_ws_responses] = useState([]);
-  const [ws_responses_key, set_ws_responses_key] = useState(0)
+type Response = { // TODO: bring this to some types folder
+  text:string;
+  key:number;
+};
 
-  const ws = useRef<WebSocket | null>(null);
+function App() {
+  const [ws_responses, set_ws_responses] = useState<Response[]>([]);
+  const ws_responses_key = useRef<number>(0)
+
+  const ws = useRef<WebSocket | null>(null); // null when uninitiliazed
   useEffect(() => {
     const socket = new WebSocket("ws://192.168.0.15:8765");
-    socket.addEventListener("message", event => { // TODO: https://developer.mozilla.org/en-US/docs/Web/API/WebSocket CODE FOR OTHER EVENTS
-      console.log(event.data)
+    const message_handler = (event) => { // TODO: https://developer.mozilla.org/en-US/docs/Web/API/WebSocket CODE FOR OTHER EVENTS
       let parse_message = JSON.parse(event.data);
-      set_ws_responses([{"text":parse_message.response,"key":ws_responses_key},...ws_responses.slice(0,21)]);
-      set_ws_responses_key( i => i+1 )
-    });
+      set_ws_responses( ws_responses => [
+        {text:parse_message.response, key:ws_responses_key.current},
+        ...ws_responses.slice(0,21)
+      ]);
+      ws_responses_key.current++;
+    }
+    socket.addEventListener("message", message_handler );
     socket.addEventListener("open", event => {
       console.log("open");
     });
     ws.current = socket;
 
-    return () => socket.close()
+    return () => socket.close();
   }, []);
 
   function send(msg: string) : void {
--- universal-remote/src/components/DisplayMessages.tsx
+++ universal-remote/src/components/DisplayMessages.tsx
@@ -1,5 +1,9 @@
-export function DisplayMessages({responses}: string[]) {
+type Response = { // todo, put this in some types folder
+  text:string;
+  key:number;
+};
 
+export function DisplayMessages({responses}: {responses: Response[]}) {
   return (
       <div className="DisplayMessages">
       <h3>Response:</h3>
