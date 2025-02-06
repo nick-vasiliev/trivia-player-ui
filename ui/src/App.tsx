68ba791ea4c9aa1db188e5c30dea66afdb8bb6b2
nick-vasiliev <nickthasim@gmail.com>
2025-02-07 00:10:28 +1100

    ws to useEffect

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,22 +1,32 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 import { ChoiceLayout } from './components/ChoiceLayout';
 import { useCookies } from 'react-cookie';
 
-const socket = new WebSocket("ws://192.168.0.13:8765")
-socket.addEventListener("open", event => {
-  socket.send("Connection established")
-});
-// Listen for messages
-socket.addEventListener("message", event => {
-  console.log("Message from server ", event.data)
-});
-
-function sendChoice(id): void {
-  socket.send(id.toString());
-}
-
 function App() {
   const [cookies, setCookie, removeCookie] = useCookies([]);
+
+  const ws = useRef<WebSocket | null>(null); // null when uninitiliazed
+  useEffect(() => {
+    const socket = new WebSocket("ws://192.168.0.15:8765");
+    const message_handler = (event) => { // TODO: https://developer.mozilla.org/en-US/docs/Web/API/WebSocket CODE FOR OTHER EVENTS
+      let parse_message = JSON.parse(event.data);
+      console.log(parse_message);
+    }
+    socket.addEventListener("message", message_handler );
+    socket.addEventListener("open", event => {
+      console.log("open"); // TODO
+    });
+    ws.current = socket;
+
+    return () => socket.close();
+  },[]);
+
+  function sendChoice(id: int): void {
+    if (!ws.current) console.log("WS not open!"); // TODO: handle this with a UI notif!
+    else ws.current.send(id.toString());
+  };
+
   const q_choices = [
     {id:0,text:"AAAAA"},
     {id:1,text:"CCCCC"},
